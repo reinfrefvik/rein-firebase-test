8c11fa359a79093eb41c0a9b511bdc84bb6f3b6f
Rein F Refvik <reinfrimann@gmail.com>
2024-07-14 20:27:24 +0200

    ggfg

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { Login } from "../components/login";
 
 const HomePage = () => {
     return (
         <div>
             <h1>content</h1>
+            <Link to="/magicitems">Magic Items</Link>
             <Login />
         </div>
     )
